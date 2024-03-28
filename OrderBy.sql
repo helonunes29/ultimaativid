236f02d39516814b9f5d43072b39b26d8c1d2035
helo <125596881+helonunes29@users.noreply.github.com>
2024-03-28 15:27:28 -0300

    Create OrderBy.sql

---
+++ OrderBy.sql
@@ -0,0 +1,8 @@
+
+SELECT * FROM `Alunos` ORDER BY Nome;
+SELECT * FROM `Alunos` ORDER BY ID;
+SELECT * FROM `Alunos` ORDER BY Idade;
+SELECT * FROM `Alunos` ORDER BY Sobrenome;
+SELECT * FROM `Alunos` ORDER BY Cidade;
+SELECT * FROM `Alunos` ORDER BY Estado;
+
